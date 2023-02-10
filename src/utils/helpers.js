5770753a41d850dcbb0887f48ea89a8c7baef891
R3V3N3NT <abhimahanta.sxsv@gmail.com>
2023-02-11 04:15:44 +0530

    currency change

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,6 +1,6 @@
 export const formatPrice = (price) => {
     return new Intl.NumberFormat('en-US', {
         style: 'currency',
-        currency: "USD"
-    }).format(price / 100);
+        currency: "INR"
+    }).format(price);
 }
